87bb8215a7e380cfdf1b8314845c7492b39212c4
May <hoangquocanh2n6@gmail.com>
2025-11-30 21:58:28 +0700

    PTIT2025R1B - Nguyên tố cùng nhau

---
+++ OLP231.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+ifstream in("input.txt");
+ofstream out("output.txt");
+
+vector<int> prime(int x){
+    vector<int> primes;
+    for(int i=2;i*i<=x;i++){
+        if(x % i == 0){
+            primes.push_back(i);
+            while(x%i == 0) x/=i;
+        }
+    }
+    if(x > 1){
+        primes.push_back(x);
+    }
+    return primes;
+}
+
+void test_case(){
+    int n,m;
+    in >> n >> m;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        in >> a[i];
+    }
+    vector<int> primes;
+    vector<bool> used(m+1);
+    vector<bool> bad(m+1);
+
+    for(int i=0;i<n;i++){
+        vector<int> ps = prime(a[i]);
+        for(int x : ps){
+            if(!used[x] && x <= m){
+                used[x] = true;
+                primes.push_back(x);
+            }
+        }
+    }
+
+    for(int x : primes){
+        for(int i = x ; i <= m ; i += x){
+            bad[i] = true;
+        }
+    }
+
+    vector<int> ans;
+    for(int i=1;i<=m;i++){
+        if(!bad[i]){
+            ans.push_back(i);
+        }
+    }
+    out << ans.size() << "\n";
+    for(int x : ans){
+        out << x << " ";
+    }
+    out << "\n";
+}
+
+int main(){
+    test_case();    
+    return 0;
+}
