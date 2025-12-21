2a3991bfa2fa2c8776dfe9b0bfe322bb73902c82
May <hoangquocanh2n6@gmail.com>
2025-12-21 21:39:29 +0700

    Chênh lệch nhỏ nhất

---
+++ CPP0201.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+void test_case(){
+    int n;
+    fin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        fin >> a[i];
+    }
+    int Min = INT_MAX;
+    for(int i=0;i<n-1;i++){
+        Min = min(Min , a[i+1] - a[i]);
+    }
+    fout << Min << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        test_case();
+    }
+    return 0;
+}
